e2e530108acf8921073d269375b3ada892c0edba
Valentin <valker@users.noreply.github.com>
2014-02-06 00:38:53 -0800

    Create timus_2001.cpp

---
+++ solved/timus_2001.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a1,b1,a2,b2,a3,b3;
+	cin >> a1 >> b1 >> a2 >> b2 >> a3 >> b3;
+	cout << (a1 - a3) << ' ' << (b1 - b2) << '\n';
+	return 0;
+}
